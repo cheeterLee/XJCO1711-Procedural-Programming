6d2af42d48c091be9505bb2596d3c4bf72532339
cheeter_Lee <johnson991210@gmail.com>
2022-09-20 10:48:21 +0800

    corrext ex2

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 
 int main(void) {
-    double accumulator = 0; // maybe manually changing this?
+    double accumulator;
     double input; 
     char operator;
 
     printf("Begin Calculations\n");
-    printf("Enter an input and an operator: ");
-    scanf("%lf %c", &input, &operator);
+    printf("PLZ enter the initial value: ");
+    scanf("%lf", &accumulator);
+    for (;;) {
+        printf("Enter an input and an operator: ");
+        scanf("%lf %c", &input, &operator);
 
-    switch (operator) {
-        case 'S':
-            accumulator = input;
-            printf("accumulator = %lf", input);
-            break;
-        case '/':
-            if (accumulator == 0) {
-                printf("The dividend can't be 0");
+        switch (operator) {
+            case 'S':
+                accumulator = input;
+                printf("accumulator = %lf", input);
                 break;
-            }
-            printf("%lf /\n= %lf", accumulator, accumulator /= input);
-            break;
-        case '-':
-            printf("%lf -\n= %lf", accumulator, accumulator -= input);
-            break;
-        case '+': 
-            printf("%lf +\n= %lf", accumulator, accumulator += input);
-            break;
-        case '*':
-            printf("%lf *\n= %lf", accumulator, accumulator *= input);
-            break;
-        case 'E':
-            printf("End of Calculations");
-            break;
-        default:
-            printf("Error, unknow operator");
+            case '/':
+                if (accumulator == 0) {
+                    printf("The dividend can't be 0");
+                    break;
+                }
+                printf("%lf /\n= %lf", accumulator, accumulator /= input);
+                break;
+            case '-':
+                printf("%lf -\n= %lf", accumulator, accumulator -= input);
+                break;
+            case '+': 
+                printf("%lf +\n= %lf", accumulator, accumulator += input);
+                break;
+            case '*':
+                printf("%lf *\n= %lf", accumulator, accumulator *= input);
+                break;
+            case 'E':
+                printf("End of Calculations");
+                break;
+            default:
+                printf("Error, unknow operator");
+        }
     }
-
 
     return 0;
 }
